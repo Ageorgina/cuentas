956089ed59678b53843c7e5d16c9804e217bf409
ageorgina <aortega@asgbpm.com>
2021-06-03 17:05:49 -0500

    correccion de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,22 +6,22 @@ export const environment = {
   gtosUrl: 'http://localhost:11200',
   firebaseConfig: false,
   firebase: {
-    // apiKey: 'AIzaSyBKYXOwuYivzLms7JPUGNLlkdRrD1b5lf8',
-    // authDomain: 'gastos-asg.firebaseapp.com',
-    // databaseURL: 'https://gastos-asg.firebaseio.com',
-    // projectId: 'gastos-asg',
-    // storageBucket: 'gastos-asg.appspot.com',
-    // messagingSenderId: '937489035289',
-    // appId: '1:937489035289:web:cb26b5b057c50ec22c7a88',
-    // measurementId: 'G-3XRB47S9KQ'
-    apiKey: "AIzaSyAvLzKw9rtXbPvQaFlhrWKpRTSLU4z_t0Q",
-    authDomain: "cuentas-ffc8b.firebaseapp.com",
-    databaseURL: "https://cuentas-ffc8b-default-rtdb.firebaseio.com",
-    projectId: "cuentas-ffc8b",
-    storageBucket: "cuentas-ffc8b.appspot.com",
-    messagingSenderId: "755363435838",
-    appId: "1:755363435838:web:7f99243c9de735ae18c86f",
-    measurementId: "G-JSYNN0MS48"
+     apiKey: 'AIzaSyBKYXOwuYivzLms7JPUGNLlkdRrD1b5lf8',
+     authDomain: 'gastos-asg.firebaseapp.com',
+     databaseURL: 'https://gastos-asg.firebaseio.com',
+     projectId: 'gastos-asg',
+     storageBucket: 'gastos-asg.appspot.com',
+     messagingSenderId: '937489035289',
+     appId: '1:937489035289:web:cb26b5b057c50ec22c7a88',
+     measurementId: 'G-3XRB47S9KQ'
+    //apiKey: "AIzaSyAvLzKw9rtXbPvQaFlhrWKpRTSLU4z_t0Q",
+    //authDomain: "cuentas-ffc8b.firebaseapp.com",
+    //databaseURL: "https://cuentas-ffc8b-default-rtdb.firebaseio.com",
+    //projectId: "cuentas-ffc8b",
+    //storageBucket: "cuentas-ffc8b.appspot.com",
+    //messagingSenderId: "755363435838",
+    //appId: "1:755363435838:web:7f99243c9de735ae18c86f",
+    //measurementId: "G-JSYNN0MS48"
   }
 
 };
